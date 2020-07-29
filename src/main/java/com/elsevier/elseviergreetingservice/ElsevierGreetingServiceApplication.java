98db882bb038cd8e5c9748c89f70df0db512b344
An Phuc Thien Nguyen <npta97@gmail.com>
2020-07-29 11:30:21 +0700

    Init project

---
+++ src/main/java/com/elsevier/elseviergreetingservice/ElsevierGreetingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elsevier.elseviergreetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsevierGreetingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElsevierGreetingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elsevier/elseviergreetingservice/ElsevierGreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsevier.elseviergreetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsevierGreetingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
