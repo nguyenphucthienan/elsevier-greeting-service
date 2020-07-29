5d2f521ea2610496b5391ee5eb98eea99655d950
An Phuc Thien Nguyen <npta97@gmail.com>
2020-07-29 15:59:54 +0700

    Add service discovery

--- src/main/java/com/elsevier/elseviergreetingservice/ElsevierGreetingServiceApplication.java
+++ src/main/java/com/elsevier/elseviergreetingservice/ElsevierGreetingServiceApplication.java
@@ -2,12 +2,13 @@ package com.elsevier.elseviergreetingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ElsevierGreetingServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ElsevierGreetingServiceApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ElsevierGreetingServiceApplication.class, args);
+  }
 }
